04e20e16144f6b173144a8e78709455ec2a71256
sangeun100 <qortkddms96@naver.com>
2022-09-09 16:16:12 +0900

    배열,객체,반복문,함수

--- JS/함수.js
+++ JS/함수.js
@@ -1,16 +1,16 @@
 // 1~10 더하기 1+2+3+...+10
 // 1~20 더하기
 
-function sum(target) {
-    let result = 0;
-for (let i = 1; i <=target; i++) {
-    target = target + i
-}
-return result;
-};
+// function sum(target) {
+//     let result = 0;
+// for (let i = 1; i <=target; i++) {
+//     target = target + i
+// }
+// return result;
+// };
 
-console.log(sum(10));
-console.log(sum(50));
+// console.log(sum(10));
+// console.log(sum(50));
 
 // 익명함수는 호이스팅(끌어올리다)를 지원하지 않음 따라서 호출 전에 정의를 해줘야함
 // const sum = function sum(target) {
@@ -23,9 +23,11 @@ console.log(sum(50));
 
 
 // 연습문제 BMI 
+
 function BMI(height, weight) {
     const meterheight = height / 100;
     const result = weight / (meterheight * meterheight);
+
     return result;
 }
-console.log(BMI(167,60));
+console.log(BMI(167, 88));
